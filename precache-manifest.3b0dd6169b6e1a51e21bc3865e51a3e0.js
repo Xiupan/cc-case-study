745688c3084a073432d7246e21f428eaec09dbe1
Xiupan <xiupan@gmail.com>
2020-05-30 18:56:40 -0500

    Updates

---
+++ precache-manifest.3b0dd6169b6e1a51e21bc3865e51a3e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b9dd26a9ac9c7c1330f5671b66fc75",
+    "url": "/cc-case-study/index.html"
+  },
+  {
+    "revision": "61cfcca568a9dfa2b047",
+    "url": "/cc-case-study/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "31685b16f4c11bb4d33d",
+    "url": "/cc-case-study/static/js/2.ce6a5f8f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cc-case-study/static/js/2.ce6a5f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61cfcca568a9dfa2b047",
+    "url": "/cc-case-study/static/js/main.f0f417b2.chunk.js"
+  },
+  {
+    "revision": "a9a3a241a4b541debff0",
+    "url": "/cc-case-study/static/js/runtime-main.f6615879.js"
+  }
+]);
